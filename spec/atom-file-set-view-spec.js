b2d62a35d35efcfc07531e9ecd1abea8b7ae20af
Jason West <jason.west@nextraq.com>
2017-05-29 10:37:47 -0400

    Initial commit

---
+++ spec/atom-file-set-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomFileSetView from '../lib/atom-file-set-view';
+
+describe('AtomFileSetView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
